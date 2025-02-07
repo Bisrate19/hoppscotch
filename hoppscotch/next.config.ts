07b0749308633f5cefd2dae73b50ef250e181d35
Bisrat Tamire <biarattamire288@gmail.com>
2025-02-07 08:44:15 +0300

    dep

--- hoppscotch/next.config.ts
+++ hoppscotch/next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/hoppscotch',
+  assetPrefix: '/hoppscotch/',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
